b65ec78689c26d7a5453c495b3cf4b43e50151b6
serranomorante <serranomorantepatricio@gmail.com>
2019-06-13 08:31:30 -0500

    avances

--- main/admin.py
+++ main/admin.py
@@ -10,6 +10,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ("in_stock",)
     search_fields = ("name",)
     prepopulated_fields = {"slug": ("name",)}
+    autocomplete_fields = ('tags',)
 
 admin.site.register(models.Product, ProductAdmin)
 
@@ -18,7 +19,6 @@ class ProductTagAdmin(admin.ModelAdmin):
     list_filter = ("active",)
     search_fields = ("name",)
     prepopulated_fields = {"slug": ("name",)}
-    autocomplete_fields = ('products',)
 
 admin.site.register(models.ProductTag, ProductTagAdmin)
 
--- main/models.py
+++ main/models.py
@@ -4,12 +4,32 @@ from django.db import models
 
 # MANAGERS
 
+class ProductTagManager(models.Manager):
+    def get_by_natural_key(self, slug):
+        return self.get(slug=slug)
+
 
 class ActiveManager(models.Manager):
     def active(self):
         return self.filter(active=True)
 
+
+
 # MODELS
+
+class ProductTag(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+
+    objects = ProductTagManager()
+
+    def __str__(self):
+        return self.name
+    
+    def natural_key(self):
+        return (self.slug,)
 
 
 class Product(models.Model):
@@ -20,6 +40,7 @@ class Product(models.Model):
     active = models.BooleanField(default=True)
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(ProductTag, blank=True)
 
     objects = ActiveManager()
 
@@ -35,12 +56,3 @@ class ProductImage(models.Model):
     thumbnail = models.ImageField(upload_to="product-thumbnails", null=True)
 
 
-class ProductTag(models.Model):
-    products = models.ManyToManyField(Product, blank=True)
-    name = models.CharField(max_length=40)
-    slug = models.SlugField(max_length=48)
-    description = models.TextField(blank=True)
-    active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
