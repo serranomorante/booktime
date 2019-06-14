0860cdcc55cf51a47a454826a02a84a5b3b3ba13
serranomorante <serranomorantepatricio@gmail.com>
2019-06-13 22:21:55 -0500

    avance

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from django.views.generic import TemplateView
-from main import views
+from django.views.generic import TemplateView, DetailView
+from main import views, models
 
 urlpatterns = [
+    path("product/<slug:slug>/", DetailView.as_view(model=models.Product), name="product"),
+    path("products/<slug:tag>/", views.ProductListView.as_view(), name="products"),
     path('contact-us/', views.ContactUsView.as_view(), name="contact_us"),
     path('about-us/', TemplateView.as_view(template_name="about_us.html"), name="about_us"),
     path('', TemplateView.as_view(template_name="home.html"), name="home"),
